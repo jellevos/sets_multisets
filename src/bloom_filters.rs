ec085db0d0dad5d445585770ebbbfde646ea9263
Jelle Vos <github@jelle-vos.nl>
2023-11-29 13:55:16 +0100

    Fix never-ending loop in Bloom filter in params

--- src/bloom_filters.rs
+++ src/bloom_filters.rs
@@ -54,13 +54,13 @@ pub fn hash_element(element: &usize, seed: u64) -> usize {
     usize::from_ne_bytes(res)
 }
 
-/// For a maximum error rate and maximum set size, returns a suitable bin count and hash count. These parameters lead to the lowest possible bin_count that assures this maximum error rate.
+/// For a maximum error rate and maximum set size, returns a suitable minimum bin count and hash count. These parameters lead to the lowest possible bin_count that assures this maximum error rate.
 pub fn gen_bloom_filter_params(max_error_rate: f64, max_set_size: usize) -> (usize, usize) {
     let mut h = 1;
     let mut previous = None;
     loop {
         let current = (-(h as f64) * (max_set_size as f64 + 0.5)
-            / (1. - max_error_rate.powf(1. / (h as f64)))
+            / (1f64.ln() - max_error_rate.powf(1. / (h as f64)))
             + 1.)
             .ceil() as usize;
 
@@ -93,11 +93,6 @@ pub fn bloom_filter_contains(bins: &[bool], element: &usize, hash_count: usize) 
     }
 
     true
-}
-
-/// Returns (`min_bin_count`, `hash_count`) that cause an error rate of at most `error_rate` when `element_count` elements are inserted into this Bloom filter.
-pub fn find_compact_params(_element_count: usize, _error_rate: f64) -> (usize, usize) {
-    todo!()
 }
 
 impl Set {
