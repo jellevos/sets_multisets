8fc8e50b49876292c2d811697c7ecd1c2a4f6230
Jelle Vos <github@jelle-vos.nl>
2025-03-31 10:25:41 +0200

    Make hasher typed so we do not have to recompile

--- src/bloom_filters.rs
+++ src/bloom_filters.rs
@@ -1,72 +1,78 @@
 use crate::{multisets::Multiset, sets::Set};
 use bytevec::ByteEncodable;
 
-// TODO: Consider supporting all hash functions at the same time
-#[cfg(all(feature = "xxh3", feature = "shake128"))]
-compile_error!("Please choose one hash function to use: xxh3 or shake128.");
-#[cfg(all(feature = "xxh3", feature = "blake3"))]
-compile_error!("Please choose one hash function to use: xxh3 or blake3.");
-#[cfg(all(feature = "shake128", feature = "blake3"))]
-compile_error!("Please choose one hash function to use: shake128 or blake3.");
-#[cfg(all(feature = "xxh3", feature = "shake128", feature = "blake3"))]
-compile_error!("Please choose one hash function to use: xxh3, shake128 or blake3.");
-
-#[cfg(feature = "xxh3")]
 use xxh3::hash64_with_seed;
-#[cfg(feature = "xxh3")]
-pub fn hash_element(element: &usize, seed: u64) -> usize {
-    let element_bytes = (*element as u64).encode::<u64>().unwrap();
-    hash64_with_seed(&element_bytes, seed) as usize
-}
 
-#[cfg(feature = "shake128")]
 use sha3::{
     digest::{ExtendableOutput, Update, XofReader},
     Shake128,
 };
-#[cfg(feature = "shake128")]
-pub fn hash_element(element: &usize, seed: u64) -> usize {
-    let element_bytes = (*element as u64).encode::<u64>().unwrap();
-    let seed_bytes = seed.encode::<u64>().unwrap();
 
-    let mut hasher = Shake128::default();
-    hasher.update(&element_bytes);
-    hasher.update(&seed_bytes);
-    let mut reader = hasher.finalize_xof();
-    let mut res = [0u8; 8];
-    reader.read(&mut res);
-
-    usize::from_ne_bytes(res)
-}
-
-#[cfg(feature = "blake3")]
-pub fn hash_element(element: &usize, seed: u64) -> usize {
-    let element_bytes = (*element as u64).encode::<u64>().unwrap();
-    let seed_bytes = seed.encode::<u64>().unwrap();
-
-    let mut hasher = blake3::Hasher::new();
-    hasher.update(&element_bytes);
-    hasher.update(&seed_bytes);
-    let mut reader = hasher.finalize_xof();
-    let mut res = [0u8; 8];
-    reader.fill(&mut res);
-
-    usize::from_ne_bytes(res)
-}
-
-#[cfg(feature = "argon2")]
 use argon2::Argon2;
-#[cfg(feature = "argon2")]
-pub fn hash_element(element: &usize, seed: u64) -> usize {
-    let element_bytes = (*element as u64).encode::<u64>().unwrap();
-    let seed_bytes = seed.encode::<u64>().unwrap();
 
-    let mut res = [0u8; 8]; // Can be any desired size
-    Argon2::default()
-        .hash_password_into(&element_bytes, &seed_bytes, &mut res)
-        .unwrap();
+pub trait ElementHasher {
+    fn hash_element(element: &usize, seed: u64) -> usize;
+}
 
-    usize::from_ne_bytes(res)
+pub struct Xxh3Hasher;
+
+pub struct Shake128Hasher;
+
+pub struct Blake3Hasher;
+
+pub struct Argon2Hasher;
+
+impl ElementHasher for Xxh3Hasher {
+    fn hash_element(element: &usize, seed: u64) -> usize {
+        let element_bytes = (*element as u64).encode::<u64>().unwrap();
+        hash64_with_seed(&element_bytes, seed) as usize
+    }
+}
+
+impl ElementHasher for Shake128Hasher {
+    fn hash_element(element: &usize, seed: u64) -> usize {
+        let element_bytes = (*element as u64).encode::<u64>().unwrap();
+        let seed_bytes = seed.encode::<u64>().unwrap();
+
+        let mut hasher = Shake128::default();
+        hasher.update(&element_bytes);
+        hasher.update(&seed_bytes);
+        let mut reader = hasher.finalize_xof();
+        let mut res = [0u8; 8];
+        reader.read(&mut res);
+
+        usize::from_ne_bytes(res)
+    }
+}
+
+impl ElementHasher for Blake3Hasher {
+    fn hash_element(element: &usize, seed: u64) -> usize {
+        let element_bytes = (*element as u64).encode::<u64>().unwrap();
+        let seed_bytes = seed.encode::<u64>().unwrap();
+
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(&element_bytes);
+        hasher.update(&seed_bytes);
+        let mut reader = hasher.finalize_xof();
+        let mut res = [0u8; 8];
+        reader.fill(&mut res);
+
+        usize::from_ne_bytes(res)
+    }
+}
+
+impl ElementHasher for Argon2Hasher {
+    fn hash_element(element: &usize, seed: u64) -> usize {
+        let element_bytes = (*element as u64).encode::<u64>().unwrap();
+        let seed_bytes = seed.encode::<u64>().unwrap();
+
+        let mut res = [0u8; 8]; // Can be any desired size
+        Argon2::default()
+            .hash_password_into(&element_bytes, &seed_bytes, &mut res)
+            .unwrap();
+
+        usize::from_ne_bytes(res)
+    }
 }
 
 /// For a maximum error rate and maximum set size, returns a suitable minimum bin count and hash count. These parameters lead to the lowest possible bin_count that assures this maximum error rate.
@@ -90,18 +96,22 @@ pub fn gen_bloom_filter_params(max_error_rate: f64, max_set_size: usize) -> (usi
     }
 }
 
-pub fn bloom_filter_indices(
+pub fn bloom_filter_indices<H: ElementHasher>(
     element: &usize,
     bin_count: usize,
     hash_count: usize,
 ) -> impl Iterator<Item = usize> + '_ {
-    (0..hash_count).map(move |seed| (hash_element(element, seed as u64) % bin_count))
+    (0..hash_count).map(move |seed| (H::hash_element(element, seed as u64) % bin_count))
 }
 
-pub fn bloom_filter_contains(bins: &[bool], element: &usize, hash_count: usize) -> bool {
+pub fn bloom_filter_contains<H: ElementHasher>(
+    bins: &[bool],
+    element: &usize,
+    hash_count: usize,
+) -> bool {
     let bin_count = bins.len();
 
-    for index in bloom_filter_indices(element, bin_count, hash_count) {
+    for index in bloom_filter_indices::<H>(element, bin_count, hash_count) {
         if !bins[index] {
             return false;
         }
@@ -111,12 +121,16 @@ pub fn bloom_filter_contains(bins: &[bool], element: &usize, hash_count: usize) 
 }
 
 impl Set {
-    pub fn to_bloom_filter(&self, bin_count: usize, hash_count: usize) -> Vec<bool> {
+    pub fn to_bloom_filter<H: ElementHasher>(
+        &self,
+        bin_count: usize,
+        hash_count: usize,
+    ) -> Vec<bool> {
         let mut bins = vec![false; bin_count];
 
         for element in &self.elements {
             for seed in 0..hash_count {
-                bins[hash_element(element, seed as u64) % bin_count] = true;
+                bins[H::hash_element(element, seed as u64) % bin_count] = true;
             }
         }
 
@@ -125,7 +139,7 @@ impl Set {
 }
 
 impl Multiset {
-    pub fn to_bloom_filter(
+    pub fn to_bloom_filter<H: ElementHasher>(
         &self,
         bin_count: usize,
         hash_count: usize,
@@ -136,7 +150,7 @@ impl Multiset {
         for (element, count) in &self.element_counts {
             for i in 0..*count {
                 for seed in 0..hash_count {
-                    bins[hash_element(&(*element * max_multiplicity + i), seed as u64)
+                    bins[H::hash_element(&(*element * max_multiplicity + i), seed as u64)
                         % bin_count] = true;
                 }
             }
@@ -146,14 +160,14 @@ impl Multiset {
     }
 }
 
-pub fn bloom_filter_retrieve_count(
+pub fn bloom_filter_retrieve_count<H: ElementHasher>(
     bins: &[bool],
     element: &usize,
     hash_count: usize,
     max_multiplicity: usize,
 ) -> usize {
     for i in 0..max_multiplicity {
-        if !bloom_filter_contains(bins, &(element * max_multiplicity + i), hash_count) {
+        if !bloom_filter_contains::<H>(bins, &(element * max_multiplicity + i), hash_count) {
             return i;
         }
     }
@@ -164,36 +178,12 @@ pub fn bloom_filter_retrieve_count(
 #[cfg(test)]
 mod tests {
     use crate::{
-        bloom_filters::{bloom_filter_contains, bloom_filter_retrieve_count},
+        bloom_filters::{bloom_filter_contains, bloom_filter_retrieve_count, Xxh3Hasher},
         multisets::Multiset,
         sets::Set,
     };
 
     use super::gen_bloom_filter_params;
-
-    #[test]
-    fn test_set_to_bloom_filter() {
-        let set = Set::new(&vec![1, 3, 4]);
-        let bloom_filter = set.to_bloom_filter(20, 2);
-
-        assert!(bloom_filter_contains(&bloom_filter, &1, 2));
-        assert!(!bloom_filter_contains(&bloom_filter, &2, 2));
-        assert!(bloom_filter_contains(&bloom_filter, &3, 2));
-        assert!(bloom_filter_contains(&bloom_filter, &4, 2));
-        assert!(!bloom_filter_contains(&bloom_filter, &5, 2));
-    }
-
-    #[test]
-    fn test_multiset_to_bloom_filter() {
-        let multiset = Multiset::new(&vec![1, 3, 4], &vec![1, 2, 1]);
-        let bloom_filter = multiset.to_bloom_filter(50, 2, 2);
-
-        assert_eq!(bloom_filter_retrieve_count(&bloom_filter, &0, 2, 2), 0);
-        assert_eq!(bloom_filter_retrieve_count(&bloom_filter, &1, 2, 2), 1);
-        assert_eq!(bloom_filter_retrieve_count(&bloom_filter, &2, 2, 2), 0);
-        assert_eq!(bloom_filter_retrieve_count(&bloom_filter, &3, 2, 2), 2);
-        assert_eq!(bloom_filter_retrieve_count(&bloom_filter, &4, 2, 2), 1);
-    }
 
     #[test]
     fn test_bf_parameters_smallrate() {
@@ -207,5 +197,147 @@ mod tests {
         let (bin_count, hash_count) = gen_bloom_filter_params(2f64.powf(-10.), 4096);
         assert_eq!(bin_count, 59102);
         assert_eq!(hash_count, 10);
+    }
+}
+
+#[cfg(test)]
+mod tests_xxh3 {
+    use crate::bloom_filters::bloom_filter_contains;
+    use crate::bloom_filters::bloom_filter_retrieve_count;
+    use crate::bloom_filters::Xxh3Hasher;
+    use crate::multisets::Multiset;
+    use crate::sets::Set;
+
+    type H = Xxh3Hasher;
+
+    #[test]
+    fn test_set_to_bloom_filter() {
+        let set = Set::new(&vec![1, 3, 4]);
+        let bloom_filter = set.to_bloom_filter::<H>(20, 2);
+
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &1, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &2, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &3, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &4, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &5, 2));
+    }
+
+    #[test]
+    fn test_multiset_to_bloom_filter() {
+        let multiset = Multiset::new(&vec![1, 3, 4], &vec![1, 2, 1]);
+        let bloom_filter = multiset.to_bloom_filter::<H>(50, 2, 2);
+
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &0, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &1, 2, 2), 1);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &2, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &3, 2, 2), 2);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &4, 2, 2), 1);
+    }
+}
+
+#[cfg(test)]
+mod tests_shake128 {
+    use super::Shake128Hasher;
+    use crate::bloom_filters::bloom_filter_contains;
+    use crate::bloom_filters::bloom_filter_retrieve_count;
+    use crate::multisets::Multiset;
+    use crate::sets::Set;
+
+    type H = Shake128Hasher;
+
+    #[test]
+    fn test_set_to_bloom_filter() {
+        let set = Set::new(&vec![1, 3, 4]);
+        let bloom_filter = set.to_bloom_filter::<H>(20, 2);
+
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &1, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &2, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &3, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &4, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &5, 2));
+    }
+
+    #[test]
+    fn test_multiset_to_bloom_filter() {
+        let multiset = Multiset::new(&vec![1, 3, 4], &vec![1, 2, 1]);
+        let bloom_filter = multiset.to_bloom_filter::<H>(50, 2, 2);
+
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &0, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &1, 2, 2), 1);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &2, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &3, 2, 2), 2);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &4, 2, 2), 1);
+    }
+}
+
+#[cfg(test)]
+mod tests_blake3 {
+    use crate::bloom_filters::bloom_filter_contains;
+    use crate::bloom_filters::bloom_filter_retrieve_count;
+    use crate::multisets::Multiset;
+    use crate::sets::Set;
+
+    use super::Blake3Hasher;
+
+    type H = Blake3Hasher;
+
+    #[test]
+    fn test_set_to_bloom_filter() {
+        let set = Set::new(&vec![1, 3, 4]);
+        let bloom_filter = set.to_bloom_filter::<H>(20, 2);
+
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &1, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &2, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &3, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &4, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &5, 2));
+    }
+
+    #[test]
+    fn test_multiset_to_bloom_filter() {
+        let multiset = Multiset::new(&vec![1, 3, 4], &vec![1, 2, 1]);
+        let bloom_filter = multiset.to_bloom_filter::<H>(50, 2, 2);
+
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &0, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &1, 2, 2), 1);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &2, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &3, 2, 2), 2);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &4, 2, 2), 1);
+    }
+}
+
+#[cfg(test)]
+mod tests_argon2 {
+    use crate::bloom_filters::bloom_filter_contains;
+    use crate::bloom_filters::bloom_filter_retrieve_count;
+    use crate::multisets::Multiset;
+    use crate::sets::Set;
+
+    use super::Argon2Hasher;
+
+    type H = Argon2Hasher;
+
+    #[test]
+    fn test_set_to_bloom_filter() {
+        let set = Set::new(&vec![1, 3, 4]);
+        let bloom_filter = set.to_bloom_filter::<H>(20, 2);
+
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &1, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &2, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &3, 2));
+        assert!(bloom_filter_contains::<H>(&bloom_filter, &4, 2));
+        assert!(!bloom_filter_contains::<H>(&bloom_filter, &5, 2));
+    }
+
+    #[test]
+    fn test_multiset_to_bloom_filter() {
+        let multiset = Multiset::new(&vec![1, 3, 4], &vec![1, 2, 1]);
+        let bloom_filter = multiset.to_bloom_filter::<H>(50, 2, 2);
+
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &0, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &1, 2, 2), 1);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &2, 2, 2), 0);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &3, 2, 2), 2);
+        assert_eq!(bloom_filter_retrieve_count::<H>(&bloom_filter, &4, 2, 2), 1);
     }
 }
